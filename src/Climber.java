966887f145f94b904c44f075ffb324629bd525cb
Kirlun12 <79675068+Merlliya@users.noreply.github.com>
2021-06-02 22:37:46 +0900

    Путь, проверка перепада, ограничение максимальной вершины
    Если максимальный перепад 3 ближайших вершин > max, то ограничить максимальной вершиной.

--- src/Climber.java
+++ src/Climber.java
@@ -3,5 +3,32 @@ public class Climber {
     private int difference3_step;
     private int length;
 
-    
+    public int getMax_height() {
+        return max_height;
+    }
+    public void setMax_height(int max_height) {
+        this.max_height = max_height;
+    }
+    public int getDifference3_step() {
+        return difference3_step;
+    }
+    public void setDifference3_step(int difference3_step) {
+        // Если максимальный перепад 3 ближайших вершин > max, то ограничить максимальной вершиной
+        if (difference3_step < max_height) {
+            this.difference3_step = difference3_step;
+        }
+        else {
+            this.difference3_step = max_height;
+        }
+    }
+    public int getLength() {
+        return length;
+    }
+    public void setLength(int length) {
+        if (length > 3) {
+            this.length = length;
+        } else {
+            System.out.println("Путь не может быть < 3 вершин!");
+        }
+    }
 }
